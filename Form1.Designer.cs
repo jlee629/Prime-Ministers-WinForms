8b8ba277ed5eb3d1e8d6c6c29a4589bd58f1a31d
Jungyu Lee <jlee629@my.centennialcollege.ca>
2022-09-13 22:12:12 -0400

    Completed Project

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace PrimeMinisters
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lstPrimeMinisters = new System.Windows.Forms.ListBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblTerm = new System.Windows.Forms.Label();
+            this.lblParty = new System.Windows.Forms.Label();
+            this.picPhoto = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picPhoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lstPrimeMinisters
+            // 
+            this.lstPrimeMinisters.FormattingEnabled = true;
+            this.lstPrimeMinisters.ItemHeight = 15;
+            this.lstPrimeMinisters.Location = new System.Drawing.Point(431, 38);
+            this.lstPrimeMinisters.Name = "lstPrimeMinisters";
+            this.lstPrimeMinisters.Size = new System.Drawing.Size(178, 319);
+            this.lstPrimeMinisters.TabIndex = 1;
+            this.lstPrimeMinisters.SelectedIndexChanged += new System.EventHandler(this.lstPrimeMinisters_SelectedIndexChanged);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Gulim", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblName.Location = new System.Drawing.Point(38, 385);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(79, 19);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "lblName";
+            // 
+            // lblTerm
+            // 
+            this.lblTerm.AutoSize = true;
+            this.lblTerm.Location = new System.Drawing.Point(38, 421);
+            this.lblTerm.Name = "lblTerm";
+            this.lblTerm.Size = new System.Drawing.Size(52, 15);
+            this.lblTerm.TabIndex = 3;
+            this.lblTerm.Text = "lblTerm";
+            // 
+            // lblParty
+            // 
+            this.lblParty.AutoSize = true;
+            this.lblParty.Location = new System.Drawing.Point(38, 453);
+            this.lblParty.Name = "lblParty";
+            this.lblParty.Size = new System.Drawing.Size(55, 15);
+            this.lblParty.TabIndex = 4;
+            this.lblParty.Text = "lblParty";
+            // 
+            // picPhoto
+            // 
+            this.picPhoto.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picPhoto.Location = new System.Drawing.Point(42, 38);
+            this.picPhoto.Name = "picPhoto";
+            this.picPhoto.Size = new System.Drawing.Size(342, 319);
+            this.picPhoto.TabIndex = 0;
+            this.picPhoto.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 505);
+            this.Controls.Add(this.lblParty);
+            this.Controls.Add(this.lblTerm);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lstPrimeMinisters);
+            this.Controls.Add(this.picPhoto);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Prime Ministers";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picPhoto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox lstPrimeMinisters;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblTerm;
+        private System.Windows.Forms.Label lblParty;
+        private System.Windows.Forms.PictureBox picPhoto;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.Json;
+using System.IO;
+
+namespace PrimeMinisters
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, PrimeMinister> primeMinisters = new Dictionary<string, PrimeMinister>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] lastNames =
+            {
+                "Trudeau", "Harper", "Martin", "Chretien", "Campbell", "Mulroney", "Turner", "Clark"
+            };
+
+            foreach (string lastName in lastNames)
+            {
+                lstPrimeMinisters.Items.Add(lastName);
+            }
+
+            PopulatedPrimeMinisters();
+
+            lstPrimeMinisters.DataSource = primeMinisters.Keys.ToList<string>();
+        }
+
+        private void lstPrimeMinisters_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            PrimeMinister primeMinisterFound = primeMinisters[lstPrimeMinisters.SelectedValue.ToString()];
+            lblName.Text = primeMinisterFound.FirstName + " " + primeMinisterFound.LastName;
+            lblTerm.Text = "Term: " + primeMinisterFound.Term;
+            lblParty.Text = "Party: " + primeMinisterFound.Party;
+            picPhoto.ImageLocation = primeMinisterFound.LastName + ".jpg";
+        }
+
+        private void PopulatedPrimeMinisters()
+        {
+            PrimeMinister pm1 = new PrimeMinister("Justin", "Trudeau", "4 November 2015 - Incumbent", "Liberal");
+            PrimeMinister pm2 = new PrimeMinister("Stephen", "Harper", "6 February 2006 - 3 November 2015", "Conservative");
+            PrimeMinister pm3 = new PrimeMinister("Paul", "Martin", "12 December 2003 - 6 February 2006", "Liberal");
+            PrimeMinister pm4 = new PrimeMinister("Jean", "Chretien", "4 November 1993 - 11 December 2003", "Liberal");
+            PrimeMinister pm5 = new PrimeMinister("Kim", "Campbell", "25 June 1993 - 3 November 1993", "Conservative");
+            PrimeMinister pm6 = new PrimeMinister("Brian", "Mulroney", "17 September 1984 - 24 June 1993", "Conservative");
+            PrimeMinister pm7 = new PrimeMinister("John", "Turner", "30 June 1984 - 16 September 1984", "Liberal");
+            PrimeMinister pm8 = new PrimeMinister("Joe", "Clark", "4 June 1979 - 2 March 1980", "Conservative");
+
+            primeMinisters.Add(pm1.LastName, pm1);
+            primeMinisters.Add(pm2.LastName, pm2);
+            primeMinisters.Add(pm3.LastName, pm3);
+            primeMinisters.Add(pm4.LastName, pm4);
+            primeMinisters.Add(pm5.LastName, pm5);
+            primeMinisters.Add(pm6.LastName, pm6);
+            primeMinisters.Add(pm7.LastName, pm7);
+            primeMinisters.Add(pm8.LastName, pm8);
+        }
+    }
+}
